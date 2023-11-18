d1b3ce3041aae22e29a7a4059252ee926933c5e5
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2023-11-18 12:43:09 +0600

    asset

--- SmartEducation.Presentation/Constant/SmartSession.cs
+++ SmartEducation.Presentation/Constant/SmartSession.cs
@@ -2,9 +2,9 @@
 {
     public class SmartSession
     {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string UserCode { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? UserCode { get; set; }
 
     }
 }
