6c7493e76c6c2b39b98edeb7e614210c0b5da3c4
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2023-11-18 12:40:15 +0600

    Layout

---
+++ SmartEducation.Domain/Model/MasterPanel/Shift.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartEducation.Domain.Model.MasterPanel
+{
+    public class Shift:BaseEntity
+    {
+        public Shift() {
+         this.isActive=false;
+        }
+        public string name { get; set; }
+        public string shortName { get; set; }
+        public DateTime? startDate { get; set; }
+        public string beginTime { get; set; }
+        public string endTime { get; set; }
+        public bool isActive { get; set; }
+
+
+
+    }
+}
