afb12302ef306ce2decfaba477c203df3b91791e
Nayeem Bhuiyan <nayeembhuiyan40@gmail.com>
2024-07-24 23:04:11 +0600

    StudentRegistration

--- SmartEducation.Domain/Model/Student/StudentRegistration.cs
+++ SmartEducation.Domain/Model/Student/StudentRegistration.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SmartEducation.Domain.Model.MasterPanel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,64 @@ namespace SmartEducation.Domain.Model.Student
 {
     public class StudentRegistration:BaseAuditEntity
     {
+        #region General_Information
+        [StringLength(100)]
+        public string studentCode { get; set; } //Life Time School Identity
+        [StringLength(150)]
+        public string firstName { get; set; }
+        [StringLength(150)]
+        public string middleName { get; set; }
+        [StringLength(150)]
+        public string lastName { get; set; }
+        [StringLength(150)]
+        public string fatherName { get; set; }
+        [StringLength(150)]
+        public string motherName { get; set; }
+        [StringLength(150)]
+        public string guardianName { get; set; }
+        #endregion
+
+        #region Local_Guardian_Info  
+        //lg=Local Guardian
+        [StringLength(150)]
+        public string lgName { get; set; }
+        [StringLength(250)]
+        public string lgAddress { get; set; }
+        [StringLength(30)]
+        public string lgContact { get; set; }
+        [StringLength(150)]
+        public string lgEmail { get; set; }
+        [StringLength(150)]
+        public string lgOccupation { get; set; }
+        public int? lgAge { get; set; }
+        #endregion
+
+        #region Accademic_Information
+        [ForeignKey("AccademicYearId")]
+        public int? AccademicYearId { get; set; }
+        public AccademicYear AccademicYear { get; set; }
+
+        public int? MeritPosition { get; set; }
+        public string ClassRoll { get; set; }
+
+        [ForeignKey("AccademicClassId")]
+        public int? AccademicClassId { get; set; }
+        public AccademicClass AccademicClass { get; set; }
+
+
+        [ForeignKey("AccademicSectionId")]
+        public int? AccademicSectionId { get; set; }
+        public AccademicSection AccademicSection { get; set; }
+
+
+        #endregion
+
+        #region Bank_Information
+        [ForeignKey("BankBranchId")]
+        public int? BankBranchId { get; set; }
+        public BankBranch BankBranch { get; set; }
+        public string bankAccountNumber { get; set; }
+
+        #endregion
     }
 }
